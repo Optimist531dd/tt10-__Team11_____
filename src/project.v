1cc6dc85a8f584b8c79d55c71b267a401acb9bc8
SophieZYQ <yz9735@nyu.edu>
2025-02-26 13:19:41 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -15,6 +15,12 @@ module tt_um_Team11 (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+wire [7:0] sum;  
+adder_8bit my_adder (
+    .A(ui_in),
+    .B(uio_in),
+    .C(sum)
+);
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
@@ -24,4 +30,12 @@ module tt_um_Team11 (
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
+endmodule
+
+module adder_8bit (
+    input  wire [7:0] A,  
+    input  wire [7:0] B,  
+    output wire [7:0] C
+);
+    assign C = A + B;
 endmodule
